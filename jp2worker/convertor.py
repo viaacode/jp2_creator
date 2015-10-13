9ef7348234503b86c06aa14ba120c7c0596cde68
Jonas Liekens <jonas.liekens@c4j.be>
2015-10-13 12:18:17 +0200

    Deleted verbose logging

--- jp2worker/convertor.py
+++ jp2worker/convertor.py
@@ -3,6 +3,5 @@ from wand.image import Image
 
 def convert(source_file, destination_file):
     with Image(filename=source_file) as img:
-        print(img.size)
         with img.convert('jp2') as img:
             img.save(filename=destination_file)
