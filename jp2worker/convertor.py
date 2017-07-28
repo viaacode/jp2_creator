725bb5968125f37bc8eee8ef910b7efeba4b831c
Dieter Van Hoof <dieter.vanhoof@c4j.be>
2017-07-28 11:49:45 +0200

    Verify file before converting it to a JP2. Don't decompress anymore since kdu_compress should be able to handle compressed TIFF files.

--- jp2worker/convertor.py
+++ jp2worker/convertor.py
@@ -2,26 +2,24 @@ from subprocess import check_output, STDOUT
 
 
 def convert(source_file, destination_file, extra_options):
-    try:
-        tojp2(source_file, destination_file, extra_options)
-    except Exception as e:
-        uncompress(source_file)
-        tojp2(source_file + "_uncompressed.tif", destination_file, extra_options)
+    verifyfile(source_file)
+    tojp2(source_file, destination_file, extra_options)
 
-def uncompress(source_file):
-    output = check_output(["convert",
-                           source_file,
-                           "+compress",
-                           source_file + "_uncompressed.tif"], stderr=STDOUT)
 
 def tojp2(source_file, destination_file, extra_options):
     output = check_output(["kdu_compress",
-                       "-i", source_file,
-                       "-o", destination_file,
-                       extra_options,
-                       "Clevels=5",
-                       "Cblk={64,64}",
-                       "Corder=RPCL",
-                       "Stiles={1024,1024}",
-                       "Clayers=12",
-                       "-rate", "-,2.5"], stderr=STDOUT)
+                           "-i", source_file,
+                           "-o", destination_file,
+                           extra_options,
+                           "Clevels=5",
+                           "Cblk={64,64}",
+                           "Corder=RPCL",
+                           "Stiles={1024,1024}",
+                           "Clayers=12",
+                           "-rate", "-,2.5"], stderr=STDOUT)
+
+
+def verifyfile(source_file):
+    output = check_output(["convert",
+                           source_file,
+                           "/tmp/test.jpg"], stderr=STDOUT)
