d759e103deba833cf545af913c95b0b3ac109d9d
Dieter Van Hoof <dieter.vanhoof@c4j.be>
2017-07-28 15:48:39 +0200

    Remove Validation step since kakadu already fails a corrupt compressed tiff.

--- jp2worker/convertor.py
+++ jp2worker/convertor.py
@@ -2,7 +2,6 @@ from subprocess import check_output, STDOUT
 
 
 def convert(source_file, destination_file, extra_options):
-    verifyfile(source_file)
     tojp2(source_file, destination_file, extra_options)
 
 
@@ -17,9 +16,3 @@ def tojp2(source_file, destination_file, extra_options):
                            "Stiles={1024,1024}",
                            "Clayers=12",
                            "-rate", "-,2.5"], stderr=STDOUT)
-
-
-def verifyfile(source_file):
-    output = check_output(["convert",
-                           source_file,
-                           "/tmp/test.jpg"], stderr=STDOUT)
